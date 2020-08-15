1c085957b3cc4a67bfeffa4339edee00ece7d27b
Louis.Su <louis.su@appimc.com>
2020-08-15 21:05:49 +0800

    toc

---
+++ static/dist/js/custom.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const id = encodeURI(entry.target.getAttribute('id')).toLowerCase();
+            if (entry.intersectionRatio > 0) {
+                document.querySelector(`li a[href="#${id}"]`).parentElement.classList.add('active');
+            } else {
+                document.querySelector(`li a[href="#${id}"]`).parentElement.classList.remove('active');
+            }
+        });
+    });
+
+    // Track all sections that have an `id` applied
+    document.querySelectorAll('h2[id], h3[id], h4[id]').forEach((section) => {
+        observer.observe(section);
+    });
+    
+});
